340b71e32f6325887318b8d14df8064484357b08
Janhvi Kulkarni <janhvi@live.unc.edu>
2019-09-01 23:51:21 -0400

    Adept Attempt

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -13,6 +13,7 @@ public class A1Adept {
 		String items[] = (String[])Array.newInstance(String.class, count);
 		double prices[] = (double[])Array.newInstance(double.class, count);
 		
+		// Create arrays with all items and their prices
 		for (int i = 0; i < count; i++) {
 			String item = scan.next();
 			items[i] = item;
@@ -23,42 +24,60 @@ public class A1Adept {
 		int customers = scan.nextInt();
 		double total = 0;
 		double x = 0;
-		double big = 0;
-		double small = 0;
+		
 		double t = 0;
 		String max = "";
 		String min = "";
+		double allTotals[] = (double[])Array.newInstance(double.class, customers);
+		
+		// Goes through each customer
 		
 		for (int i = 0; i < customers; i++) {
 			String first = scan.next();
 			String last = scan.next();
-			int c = scan.nextInt();
-			
+			int c = scan.nextInt(); // Count for how many items bought
+			total = 0;
+			// Goes through each item the customer bought
 			for (int j = 0; j < c; j++) {
 				int quantity = scan.nextInt();
 				String name = scan.next();
 				t = 0;
+				// Goes through items and prices arrays
 				for (int k = 0; k < items.length; k++) {
-					x = 0;
 					if (items[k] == name) {
 						x += prices[k] * quantity;
 					}
-					t += x;
 				}
-				total += t;
+				total += x; // Total price of all items bought
 			}
-			if (total > big) {
-				big = total;
-				max = first + " " + last;
-			} else if (total < small) {
-				small = total;
-				min = first + " " + last;
+			allTotals[i] = total;
+		}
+		
+		double big = allTotals[0];
+		double small = allTotals[0];
+		
+		for (int i = 1; i < allTotals.length; i++) {
+			if (allTotals[i] < small) {
+				small = allTotals[i];
 			}
+			
+		for (int j = 1; j < allTotals.length; j++) {
+			if (allTotals[i] > big) {
+				big = allTotals[i];
+			}
+		
+		for (int k = 0; k < allTotals.length; k++) {
+			t += allTotals[k];
 		}
 		
 		System.out.println("Biggest: " + max + "(" + big + ")");
 		System.out.println("Smallest: " + min + "(" + small + ")");
 		System.out.println("Average: " + t/customers);
 		scan.close();
+		}
+		
+		}
 	}
 }
+
+		
