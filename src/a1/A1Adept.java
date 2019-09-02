bdadb085f40566518c6d7aef37a1b559ad0d9e29
Janhvi Kulkarni <janhvi@live.unc.edu>
2019-09-02 00:10:48 -0400

    Attempt Adept 2

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,8 +10,8 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 		// Your code follows here.
 		int count = scan.nextInt();
-		String items[] = (String[])Array.newInstance(String.class, count);
-		double prices[] = (double[])Array.newInstance(double.class, count);
+		String items[] = new String[count];
+		double prices[] = new double[count];
 		
 		// Create arrays with all items and their prices
 		for (int i = 0; i < count; i++) {
@@ -21,58 +21,62 @@ public class A1Adept {
 			prices[i] = price;
 		}
 		
-		int customers = scan.nextInt();
-		double total = 0;
-		double x = 0;
+		int customerCount = scan.nextInt();
 		
-		double t = 0;
-		String max = "";
-		String min = "";
-		double allTotals[] = (double[])Array.newInstance(double.class, customers);
+		double customerTotal;
+		double oneItemPrice = 0;
+		double all = 0;
+		String maxName = "";
+		String minName = "";
+		double allTotals[] = new double[customerCount];
+		String customerNames[] = new String[customerCount];
 		
 		// Goes through each customer
 		
-		for (int i = 0; i < customers; i++) {
+		for (int i = 0; i < customerCount; i++) {
 			String first = scan.next();
 			String last = scan.next();
-			int c = scan.nextInt(); // Count for how many items bought
-			total = 0;
+			int itemCount = scan.nextInt(); // Count for how many items bought
+			customerTotal = 0;
 			// Goes through each item the customer bought
-			for (int j = 0; j < c; j++) {
+			for (int j = 0; j < itemCount; j++) {
 				int quantity = scan.nextInt();
 				String name = scan.next();
-				t = 0;
 				// Goes through items and prices arrays
 				for (int k = 0; k < items.length; k++) {
+					oneItemPrice = 0;
 					if (items[k] == name) {
-						x += prices[k] * quantity;
+						oneItemPrice += prices[k] * quantity;
 					}
 				}
-				total += x; // Total price of all items bought
+				customerTotal += oneItemPrice; // Total price of all items bought
 			}
-			allTotals[i] = total;
+			customerNames[i] = first + " " + last;
+			allTotals[i] = customerTotal;
 		}
 		
-		double big = allTotals[0];
-		double small = allTotals[0];
+		double maxTotal = allTotals[0];
+		double minTotal = allTotals[0];
 		
-		for (int i = 1; i < allTotals.length; i++) {
-			if (allTotals[i] < small) {
-				small = allTotals[i];
+		for (int i = 0; i < allTotals.length; i++) {
+			if (allTotals[i] < minTotal) {
+				minTotal = allTotals[i];
+				minName = customerNames[i];
 			}
 			
-		for (int j = 1; j < allTotals.length; j++) {
-			if (allTotals[i] > big) {
-				big = allTotals[i];
+		for (int j = 0; j < allTotals.length; j++) {
+			if (allTotals[j] > maxTotal) {
+				maxTotal = allTotals[j];
+				maxName = customerNames[j];
 			}
 		
 		for (int k = 0; k < allTotals.length; k++) {
-			t += allTotals[k];
+			all += allTotals[k];
 		}
 		
-		System.out.println("Biggest: " + max + "(" + big + ")");
-		System.out.println("Smallest: " + min + "(" + small + ")");
-		System.out.println("Average: " + t/customers);
+		System.out.println("Biggest: " + maxName + "(" + maxTotal + ")");
+		System.out.println("Smallest: " + minName + "(" + minTotal + ")");
+		System.out.println("Average: " + all/customerCount);
 		scan.close();
 		}
 		
